5b75a64b5911afefd21b686ec519e1b81617e7f8
Mario Reder <mario.reder@pm.me>
2025-11-27 10:15:50 +0100

    fix: migration

--- api/prisma/migrations/20251125145648_add_symbol_list/migration.sql
+++ api/prisma/migrations/20251125145648_add_symbol_list/migration.sql
@@ -1,8 +1,28 @@
 -- AlterTable
-ALTER TABLE "Dex" ADD COLUMN     "symbolList" TEXT;
+ALTER TABLE
+  "Dex"
+ADD
+  COLUMN IF NOT EXISTS "symbolList" TEXT;
 
 -- CreateIndex
-CREATE INDEX "UsedTransactionHash_dexId_idx" ON "UsedTransactionHash"("dexId");
+CREATE INDEX IF NOT EXISTS "UsedTransactionHash_dexId_idx" ON "UsedTransactionHash"("dexId");
 
 -- AddForeignKey
-ALTER TABLE "UsedTransactionHash" ADD CONSTRAINT "UsedTransactionHash_dexId_fkey" FOREIGN KEY ("dexId") REFERENCES "Dex"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+DO $ $ BEGIN IF NOT EXISTS (
+  SELECT
+    1
+  FROM
+    pg_constraint
+  WHERE
+    conname = 'UsedTransactionHash_dexId_fkey'
+) THEN
+ALTER TABLE
+  "UsedTransactionHash"
+ADD
+  CONSTRAINT "UsedTransactionHash_dexId_fkey" FOREIGN KEY ("dexId") REFERENCES "Dex"("id") ON DELETE
+SET
+  NULL ON UPDATE CASCADE;
+
+END IF;
+
+END $ $;
